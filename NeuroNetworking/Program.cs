067e454d3f6af81085c24d584c0cae35315969e8
Александров Евгений Владимирович <2160@inbox.ru>
2016-07-23 19:13:05 +0300

    Add project files.

---
+++ NeuroNetworking/Program.cs
@@ -0,0 +1,30 @@
+﻿using Emgu.CV;
+using Emgu.CV.UI;
+using System;
+using System.Windows.Forms;
+
+namespace NeuroNetworking
+{
+    static class Program
+    {
+        private static ImageViewer _viewerForm;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            _viewerForm = new ImageViewer();
+            IniteCupture();
+        }
+        private static void IniteCupture()
+        {
+            var capture = new Capture();
+            Application.Idle += delegate (Object sender, EventArgs e)
+            {
+                _viewerForm.Image = capture.QueryFrame();
+            };
+            _viewerForm.ShowDialog();
+        }
+    }
+}
